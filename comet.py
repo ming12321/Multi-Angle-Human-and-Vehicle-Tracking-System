bd914e6ceda82eede5f445cae44f36306e3a450e
ming12321 <justin20040711@gmail.com>
2025-03-27 22:31:24 +0800

    rename

--- comet.py
+++ comet.py
@@ -1,18 +1,20 @@
+# -*- coding: utf-8 -*-
 import comet_ml
 
-comet_ml.login(project_name="comet-example-yolo11-coco128")
+comet_ml.login(project_name="multi_angle_human_and_vehicle_tracking_system")
 
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo11n.pt")
+model = YOLO("yolo11x.pt")
 
 # Train the model
 results = model.train(
-    data="coco8.yaml",
-    project="comet-example-yolo11-coco128",
+    data="C:\Users\justi\OneDrive\орн▒\Multi-Angle-Human-and-Vehicle-Tracking-System\First_model\data.yaml",
+    project="multi_angle_human_and_vehicle_tracking_system",
     batch=32,
     save_period=1,
     save_json=True,
     epochs=3,
+    imgsz=640,
 )
